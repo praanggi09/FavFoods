02177f5475884ed93f15c4bce4ee1ecb209cc7ac
pitsburs <praanggi09@gmail.com>
2024-07-11 10:16:37 +0700

    Initial Commit

---
+++ FavFoods/FavFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavFoodsApp.swift
+//  FavFoods
+//
+//  Created by Anggi on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FavFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
